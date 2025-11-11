3ab0df39e241a75641ffd1f8b558b3618640c594
Marcelo Silva <mhps.study@gmail.com>
2025-11-11 19:56:29 -0300

    Feat: adicionando pasta js e tag script no html

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/ Fazendo a lógica de adicionar a lista
+/*
+Lista de Tarefas
+1- Saber quando o botão de adicionar for acionado [];
+2- Pegar a informaÇão do input [];
+3- Exibir o texto na tela [];
+4- Deletar a tarefa da tela (Ao clicar no botão deletar) [].
+*/
