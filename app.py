df758ccd9b7df8903deff224d590f8b3034ac587
Marcelo Silva <mhps.study@gmail.com>
2025-11-27 15:18:59 -0300

    Fix: corrigindo tamanho de hash password

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(50))
+    password = db.Column(db.String(200))
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
